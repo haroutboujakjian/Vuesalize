66e731821117759c91bc7bef2fa2577ee901860f
Harout Boujakjian <haroutb719@gmail.com>
2022-07-27 09:00:18 -0400

    BREAKING CHANGE: remove hierarchical edge bundling chart and docs section

--- src/main.js
+++ src/main.js
@@ -5,17 +5,16 @@ import LineChart from "./LineChart";
 import GroupedBarChart from "./GroupedBarChart";
 import LoaderSpinning from "./LoaderSpinning";
 import Network from "./Network";
-import HierarchicalEdgeBundling from "./HierarchicalEdgeBundling";
 import AreaChart from "./AreaChart";
 import ScatterPlot from "./ScatterPlot"
 
 const Components = {
-	StackedBarChart, BaseLegend, LineChart, GroupedBarChart, LoaderSpinning, Network,
-	HierarchicalEdgeBundling, AreaChart, ScatterPlot
+    StackedBarChart, BaseLegend, LineChart, GroupedBarChart, LoaderSpinning, Network,
+    AreaChart, ScatterPlot
 }
 
 Object.keys(Components).forEach(name => {
-	Vue.component(name, Components[name])
+    Vue.component(name, Components[name])
 })
 
 export default Components;
